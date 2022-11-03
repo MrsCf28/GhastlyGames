9b235523085d763602110182cb908d4bfd3f58f8
MrsCf28 <rebeccaflynn28@outlook.com>
2022-11-03 10:27:25 +0000

    allow the delete button to appear to a particular user if they are signed in

--- src/components/2.SingleReview/CommentCard.jsx
+++ src/components/2.SingleReview/CommentCard.jsx
@@ -1,10 +1,27 @@
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+
 export default function CommentCard({ comment }) {
+    const { user } = useContext(UserContext);
+
+    function handleDeleteClick() {}
+
     return (
         <section className="CommentCard ">
-            <p>
-                User: <b>{comment.author}</b>, Votes:{' '}
-                <b>{comment.votes}</b>
-            </p>
+            <div className="commentTopLine">
+                <p>
+                    User: <b>{comment.author}</b>, Votes:{' '}
+                    <b>{comment.votes}</b>
+                </p>
+                {user.signedIn && user.name === comment.author && (
+                    <button
+                        id="deleteComment"
+                        onClick={handleDeleteClick}
+                    >
+                        Delete
+                    </button>
+                )}
+            </div>
             <p>Date: {comment.created_at}</p>
             <p>Comment: {comment.body}</p>
         </section>
