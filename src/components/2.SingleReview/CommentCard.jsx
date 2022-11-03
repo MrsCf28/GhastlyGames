cbd374063b062c7687b9a5d18e3f266dd9ef4ab7
MrsCf28 <rebeccaflynn28@outlook.com>
2022-11-03 15:53:22 +0000

    changes message once comment is deleted via api

--- src/components/2.SingleReview/CommentCard.jsx
+++ src/components/2.SingleReview/CommentCard.jsx
@@ -9,37 +9,48 @@ export default function CommentCard({ comment }) {
     const [nowDeleted, setNowDeleted] = useState(false);
 
     function handleDeleteClick() {
-        setCommentDeleted(true)
-        setErr(null)
-        deleteComment(comment.comment_id).then(() => {
-            setNowDeleted(true);
-        }).catch(err => {
-            setCommentDeleted(false)
-            setErr('Something went wrong, please try again');
-        });
+        setCommentDeleted(true);
+        setErr(null);
+        deleteComment(comment.comment_id)
+            .then(() => {
+                setNowDeleted(true);
+            })
+            .catch(err => {
+                setCommentDeleted(false);
+                setErr('Something went wrong, please try again');
+            });
     }
 
     return (
         <section className="CommentCard ">
             {err !== null && <p>{err}</p>}
-            {!commentDeleted && <>
-            <div className="commentTopLine">
-                <p>
-                    User: <b>{comment.author}</b>, Votes:{' '}
-                    <b>{comment.votes}</b>
-                </p>
-                {user.signedIn && user.name === comment.author && (
-                    <button
-                        id="deleteComment"
-                        onClick={handleDeleteClick}
-                    >
-                        Delete
-                    </button>
-                )}
-            </div>
-            <p>Date: {comment.created_at}</p>
-            <p>Comment: {comment.body}</p></>}
-            {commentDeleted && !nowDeleted && <p>Your comment is being deleted, please wait</p>}
+            {!commentDeleted && (
+                <>
+                    <div className="commentTopLine">
+                        <p>
+                            User: <b>{comment.author}</b>, Votes:{' '}
+                            <b>{comment.votes}</b>
+                        </p>
+                        {user.signedIn &&
+                            user.name === comment.author && (
+                                <button
+                                    id="deleteComment"
+                                    onClick={handleDeleteClick}
+                                >
+                                    Delete
+                                </button>
+                            )}
+                    </div>
+                    <p>Date: {comment.created_at}</p>
+                    <p>Comment: {comment.body}</p>
+                </>
+            )}
+            {commentDeleted &&
+                (!nowDeleted ? (
+                    <p>Your comment is being deleted, please wait</p>
+                ) : (
+                    <p>Your comment has been deleted</p>
+                ))}
         </section>
     );
 }
